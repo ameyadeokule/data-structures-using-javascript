6cc14ec93392d04b4c232354a84a444bc1a5b25b
Ameya Deokule <deokuleameya@gmail.com>
2021-06-19 22:39:49 +0530

    longest word in an string using regex

--- string operations/longest-word-in-string.js
+++ string operations/longest-word-in-string.js
@@ -13,3 +13,28 @@ console.log(
 			' It tests for longest word in the string which is supposed to be mississippi',
 	),
 )
+
+//Longest word using regex
+const longestUsingRegex = (str) => {
+	// Split the string using regex
+	str = str.match(/[a-zA-Z0-9]+/gi)
+	// Creating a empty string to store largest word
+	let largest = ''
+
+	// Creating a for...loop to iterate over the array
+	for (let i = 0; i < str.length; i++) {
+		// If the i'th item is greater than largest string
+		// then overwrite the largest string with the i'th value
+		if (str[i].length > largest.length) {
+			largest = str[i]
+		}
+	}
+	return largest
+}
+
+console.log(
+	longestUsingRegex(
+		'Hello guys this is a test' +
+			' It tests for longest word in the string which is supposed to be mississippi',
+	),
+)
