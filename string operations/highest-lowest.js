afb67cafe2276f5c1be5862ed68e4190af580426
Ameya Deokule <deokuleameya@gmail.com>
2021-06-27 20:20:00 +0530

    added sum of lowest and highest number in a string

--- string operations/highest-lowest.js
+++ string operations/highest-lowest.js
@@ -1,5 +1,7 @@
+const sortString = (string) => string.split(' ').sort((a, b) => a - b)
+
 const highestAndLowest = (numberString) => {
-	const numberArray = numberString.split(' ').sort((a, b) => a - b)
+	const numberArray = sortString(numberString)
 	return [numberArray[0], numberArray[numberArray.length - 1]]
 }
 
@@ -8,3 +10,21 @@ const [lowest, highest] = highestAndLowest('5 6 1 2 77 3 12')
 console.log(lowest, ' is the lowest number')
 
 console.log(highest, ' is the highest number')
+
+//Sum of the highest and lowest numbers in the string
+
+const sumOfLowest = (numberString) => {
+	const [firstNumber, secondNumber] = sortString(numberString)
+
+	return +firstNumber + +secondNumber
+}
+
+console.log(sumOfLowest('5 6 1 2 77 3 12'))
+
+const sumOfHighest = (numberString) => {
+	const numberArray = sortString(numberString)
+	const length = numberArray.length
+	return +numberArray[length - 1] + +numberArray[length - 2]
+}
+
+console.log(sumOfHighest('5 6 1 2 77 3 12'))
